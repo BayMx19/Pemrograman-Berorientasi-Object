717f42e431ae3932554683f66605e79f00cdb078
Isa Iman <72482384+BayMx19@users.noreply.github.com>
2021-11-23 16:06:02 +0700

    Create interface.php

---
+++ Praktikum 6/No2/interface.php
@@ -0,0 +1,14 @@
+<!-- This page was c0ded by Isa Iman Muhammad - 20051397019 -->
+
+<?php 
+
+interface Flyer {
+    public function takeOff();
+    public function land();
+    public function fly();
+}
+
+interface Sailer {
+    public function dock();
+    public function cruise();
+}
